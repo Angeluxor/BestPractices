e86387ccc31fc4b340cb0b286c072288bc17193b
Angeluxor <idmiguelangel123@gmail.com>
2022-04-19 21:30:48 -0500

    se crea el metodo para filtrar por año

---
+++ src/main/java/com/sofka/GetSongsData.java
@@ -0,0 +1,137 @@
+package com.sofka;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import static com.sofka.Playlist.createNewPlaylist;
+import static com.sofka.Playlist.getPlaylistCollection;
+import static com.sofka.Song.getSongList;
+
+public class GetSongsData {
+
+    static List<Song> songsList;
+
+    public static void main(String[] args) throws IOException {
+        getSongData();
+        menu();
+        System.out.println("""
+                         
+                                Your music library:
+                         
+                """);
+
+    }
+
+    public static void menu() {
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+        int option;
+        while (!exit) {
+            System.out.println("""
+                    Welcome to your song library, please select an option:
+                    1. List of all available songs
+                    2. Create playlists
+                    3. Filter songs by genre
+                    4. Filter songs by year
+                    5. Sort songs by length
+                    6. Sort songs by date
+                    7. Show playlists
+                    8. Exit
+                    """);
+            option = scanner.nextInt();
+            switch (option) {
+                case 1 -> songsList.forEach(System.out::println);
+                case 2 -> createNewPlaylist();
+                case 3 -> genderFilter();
+                case 4 -> yearFilter();
+                case 7 -> {
+                    List<Playlist> playlistCollection = getPlaylistCollection();
+                    playlistCollection.forEach(System.out::println);
+                }
+                case 8 -> exit = true;
+                default -> System.out.println("Sorry at the moment only the options between 1 and 7 are available");
+            }
+
+        }
+    }
+
+    private static void genderFilter() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("""
+                The following genres can be filtered:
+                1. Rock
+                2. Cantautor
+                3. Bolero
+                4. Rap
+                5. Back
+                """);
+        int option2 = scanner.nextInt();
+        scanner.nextLine();
+        switch (option2) {
+            case 1 -> {
+                List<Song> filteredList1 = songsList.stream()
+                        .filter(Song -> Song.getGender().equals("Rock"))
+                        .collect(Collectors.toList());
+                System.out.println(filteredList1);
+            }
+            case 2 -> {
+                List<Song> filteredList2 = songsList.stream()
+                        .filter(Song -> Song.getGender().equals("Cantautor"))
+                        .collect(Collectors.toList());
+                System.out.println(filteredList2);
+            }
+            case 3 -> {
+                List<Song> filteredList3 = songsList.stream()
+                        .filter(Song -> Song.getGender().equals("Bolero"))
+                        .collect(Collectors.toList());
+                System.out.println(filteredList3);
+            }
+            case 4 -> {
+                List<Song> filteredList4 = songsList.stream()
+                        .filter(Song -> Song.getGender().equals("Rap"))
+                        .collect(Collectors.toList());
+                System.out.println(filteredList4);
+            }
+            case 5 -> menu();
+            default -> System.out.println("Sorry at the moment only the options between 1 and 5 are available");
+        }
+    }
+    private static void yearFilter() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("""
+                The next options are available:
+                1. Filter by your desired year\040
+                2. Go back to the menu
+                
+                """);
+        int option3 = scanner.nextInt();
+        scanner.nextLine();
+        switch (option3) {
+            case 1 -> {
+                System.out.println("Type the year you want to filter");
+                String year = scanner.next();
+
+                List<Song> yearFilter = songsList.stream()
+                        .filter(Song -> Song.getDate().equals(year))
+                        .collect(Collectors.toList());
+                System.out.println(yearFilter);
+            }
+            case 2 -> menu();
+
+            default -> System.out.println("Sorry at the moment only the options between 1 and 2 are available");
+        }
+    }
+
+
+    private static void getSongData() throws IOException {
+        List<Map<String, String>> dataList = CsvUtils.getDataCsv("songs");
+        songsList = getSongList(dataList);
+    }
+
+    public static List<Song> getSongsList() {
+        return songsList;
+    }
+}
--- src/main/java/com/sofka/Playlist.java
+++ src/main/java/com/sofka/Playlist.java
@@ -2,19 +2,106 @@ package com.sofka;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
+import static com.sofka.GetSongsData.getSongsList;
+import static com.sofka.GetSongsData.menu;
 
 public class Playlist {
-    private final List<Song> songs;
+    private static final List<Playlist> playlistCollection = new ArrayList<>();
+    private static List<Song> songs = new ArrayList<>();
+    private static String name;
+
+    public Playlist(List<Song> songs, String name) {
+        Playlist.songs = songs;
+        Playlist.name = name;
+    }
 
     public Playlist() {
-        this.songs = new ArrayList<Song>();
     }
 
-    public void addSong(Song song) {
+    public static Playlist createNewPlaylist() {
+
+        int choice;
+        String name;
+        int id;
+        while (true) {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("""
+                        Do you want to create a new playlist?
+                        1. Yes
+                        2. No
+                        """);
+                choice = (Integer.parseInt(scanner.nextLine()));
+                if (choice == 1) {
+                    System.out.println("Please type a name for the new playlist");
+                    name = scanner.nextLine();
+                    addSong();
+                    Playlist newPlaylist = new Playlist(songs, name);
+                    playlistCollection.add(newPlaylist);
+                    return newPlaylist;
+                } else if (choice == 2) {
+                    System.out.println("Returning to main menu");
+                    menu();
+                } else {
+                    System.out.println("Invalid answer");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid answer, the value must be a number");
+            }
+
+        }
+
+    }
+
+    public static void addSong() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please type the Id of the song that you want to add");
+        int id = (Integer.parseInt(scanner.nextLine()));
+        id = id - 1;
+        Song song = getSongsList().get(id);
         songs.add(song);
+        System.out.println("""
+                Do you want add another song?
+                1. Yes
+                2. No
+                """);
+        int choice = (Integer.parseInt(scanner.nextLine()));
+        if (choice == 1) {
+            addSong();
+        } else if (choice == 2) {
+            System.out.println("Returning to main menu");
+            //menu();
+        } else {
+            System.out.println("Invalid answer");
+        }
+
     }
 
-    public List<Song> getSongs() {
+    public static List<Song> getSongs() {
         return songs;
+    }
+
+    public void setSongs(List<Song> songs) {
+        Playlist.songs = songs;
+    }
+
+    public static List<Playlist> getPlaylistCollection() {
+        return playlistCollection;
+    }
+
+    @Override
+    public String toString() {
+        return "Playlist: " + name + "\n" +
+                "Songs: " + "\n" + songs + "\n";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        Playlist.name = name;
     }
 }
